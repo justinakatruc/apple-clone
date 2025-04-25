41012979ae80a0e422dabd02bd5e2fc489d420d2
Truc Minh Nguyen <minhtruc103@gmail.com>
2025-04-25 13:09:48 +0700

    Added Welcome frame and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
+"use client";
+import React from "react";
+import NavBar from "@/ui/NavBar";
 
 export default function Home() {
+  const [showInfo, setShowInfo] = React.useState(true);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="text-2xl font-bold text-center">
-        Apple Clone With Next.js
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <div className={`flex flex-col justify-around overflow-hidden + ${showInfo ? "h-svh" : "h-0 opacity-0"} + transition-height duration-1000 ease-in-out`}>
+        <div className="text-2xl font-bold text-center">
+          Apple Clone With Next.js
+        </div>
+        <div className="flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-bold cursor-pointer animate-(--zoom-in-out) hover:text-blue-900" onClick={(prev) => setShowInfo(!prev)}>
+            Welcome to the Apple Clone
+          </h1>
+          <p className="text-lg text-gray-600">
+            This is a simple clone of Apple's website using Next.js.
+          </p>
+        </div>
+        <div className="text-sm text-gray-500 text-center">
+          © 2025 Truc Minh Nguyen. All rights reserved.
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center gap-8">
-        <h1 className="text-4xl font-bold">Welcome to the Apple Clone</h1>
-        <p className="text-lg text-gray-600">
-          This is a simple clone of Apple's website using Next.js.
-        </p>
+      {!showInfo && 
+      <div className="h-lvh">
+        <NavBar />
       </div>
-      <div className="text-sm text-gray-500 text-center">
-        © 2025 Truc Minh Nguyen. All rights reserved.
-      </div>
+      }
     </div>
   );
 }
